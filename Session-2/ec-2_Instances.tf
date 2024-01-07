258b3b366b9164bd94bc987052329b043dca8e14
SUDHAKARREDDYPONUGOTI <ponugotisudhakar468@gmail.com>
2024-01-07 21:24:45 +0530

    session-2

---
+++ Session-2/ec-2_Instances.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb" #devops-practice
+  instance_type = "t2.micro"
+  
+
+  tags = {
+    Name = "MyTerraform"
+  }
+}
+
+resource "aws_security_group" "allow_tls_mysg" {
+  name        = "allow_tls_mysg"
+  description = "Allow TLS inbound traffic rule"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "inbound rule"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
