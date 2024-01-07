09bef29c53e00c87a2dceb2221639acf2a603211
SUDHAKARREDDYPONUGOTI <ponugotisudhakar468@gmail.com>
2024-01-07 21:27:54 +0530

    terraform 1

--- Session-2/ec-2_Instances.tf
+++ Session-2/ec-2_Instances.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #devops-practice
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls_mysg]
   
 
   tags = {
