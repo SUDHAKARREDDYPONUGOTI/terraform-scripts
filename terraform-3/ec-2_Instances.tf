f9790eb962c17b24dbfc1a854938937a7ab73ec1
SUDHAKARREDDYPONUGOTI <ponugotisudhakar468@gmail.com>
2024-01-08 12:06:43 +0530

    terraform 1

--- terraform-3/ec-2_Instances.tf
+++ terraform-3/ec-2_Instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = var.ami #devops-practice
-  instance_type = instance_type
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_tls_mysg.id]
 
   tags = {
